aacace3edee9c9870d97b4a39c676c2c0585ac0a
tam-en <tamis@leftelbow.com>
2019-02-06 14:07:33 -0800

    added a possible fix for CORS error over routes

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ function fromRequest(req){
   return null;
 }
 
+/// CORS fix?
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//////./
+
 // Controllers
 // Remember to pass the JWT_SECRET to ExpressJWT (it will break without it!)
 // NOTE on ExpressJWT: The unless portion is only needed if you need exceptions
