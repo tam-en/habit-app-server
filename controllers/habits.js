75b1a8c675de9973b98b6028c597b57595229da6
tam-en <tamis@leftelbow.com>
2019-02-07 15:13:51 -0800

    adding habit id parameter on completions route

--- controllers/habits.js
+++ controllers/habits.js
@@ -49,9 +49,8 @@ router.put('/:userid', (req, res) => {
 })
 
 // Get the daily completion data to store as a state in the HabitDetail component
-router.get('/completion/:userid', (req, res) => {
-    console.log("bodyToSend=", bodyToSend)
-    db.Habit.findById(bodyToSend.habit.id)
+router.get('/completion/:userid/:habitId', (req, res) => {
+    db.Habit.findById(req.params.habitId)
     .then(habit => {
         res.status(200).send(habit.days)
     })
