b186095b10823707fad3b1e57119835f6539beda
tam-en <tamis@leftelbow.com>
2019-02-07 09:49:59 -0800

    changed req.body completions route

--- controllers/habits.js
+++ controllers/habits.js
@@ -66,7 +66,7 @@ router.put('/completions/:userid', (req, res) => {
         } else {
             console.log("Creating new day in completions array")
             // today's completion doesn't exist yet, need to be created and pushed
-            newDaysArray = newDaysArray.push({req.body})
+            newDaysArray = newDaysArray.push(req.body)
         }
         db.findOneAndUpdate({ id: habit.id }, {days: newDaysArray})
         .then(habit => {
